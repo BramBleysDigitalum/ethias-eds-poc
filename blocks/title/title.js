47a4479446e23f3abc4ce4fc3f4c55dff255c5d7
brambleys <bram.bleys@digitalum.eu>
2024-08-16 15:14:26 +0200

    Ethias page: Add styling option for cards

--- blocks/title/title.js
+++ blocks/title/title.js
@@ -2,7 +2,6 @@ export default function decorate(block) {
   const [titleTextValueWrapper, typeWrapper] = block.children;
 
   const type = typeWrapper.textContent.trim();
-  console.log(type);
   typeWrapper.remove();
   const title = document.createElement(type);
   title.textContent = titleTextValueWrapper.textContent.trim();
